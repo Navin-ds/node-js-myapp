be04d924d0c98fcd892aaf20dcdcc262f86baf3d
Navin-ds <navinnavinagarjunapu1422@gmail.com>
2021-06-29 13:11:44 +0000

    intial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const server = express();
+const posts = require("./routes/posts.js");
+
+const body_parser = require("body-parser");
+
+// parse JSON (application/json content-type)
+server.use(body_parser.json());
+
+const port = 3000;
+
+// << db setup >>
+const db = require("./db.js/db");
+const dbName = "TrySmartStudy";
+const collectionCourse = "courses";
+
+// << db init >>
+db.initialize(
+  dbName,
+  collectionCourse,
+  function (dbCollection) {
+    // successCallback
+
+    // << db CRUD routes >>
+    server.get("/course/:id", (request, response) => {
+      const courseId = request.params.id;
+      console.log(courseId);
+
+      dbCollection.findOne({ id: courseId }, (error, result) => {
+        if (error) throw error;
+        // return item
+        response.json(result);
+      });
+    });
+  },
+  function (err) {
+    // failureCallback
+    throw err;
+  }
+);
+
+const collectionStream = "streams";
+
+// << db init >>
+db.initialize(
+  dbName,
+  collectionStream,
+  function (dbCollection) {
+    // successCallback
+
+    // << db CRUD routes >>
+    server.get("/stream/:id", (request, response) => {
+      const streamId = request.params.id;
+      console.log(streamId);
+
+      dbCollection.findOne({ id: streamId }, (error, result) => {
+        if (error) throw error;
+        // return item
+        response.json(result);
+      });
+    });
+  },
+  function (err) {
+    // failureCallback
+    throw err;
+  }
+);
+
+server.listen(port, () => {
+  console.log(`Server listening at ${port}`);
+});
